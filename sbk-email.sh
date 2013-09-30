e322c5816cd2fc597ec3c9df746c94d72550907c
oatley <oatleywillisa@gmail.com>
2013-09-30 11:34:02 -0400

    Modified email script

--- sbk-email.sh
+++ sbk-email.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sbk --send-report --report-email=oatleywillisa@gmail.com --report-date=$(/bin/date +'\%Y-\%m-\%d')
+sbk --send-report --report-email=user@email.com --report-date=$(/bin/date +'\%Y-\%m-\%d')
